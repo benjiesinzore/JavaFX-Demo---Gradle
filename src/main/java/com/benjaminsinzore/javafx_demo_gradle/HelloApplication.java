802b8de95b54d6d72ba2c7198e0c6c013800a778
benjiesinzore <benjaminsinzore@gmail.com>
2022-11-27 13:41:27 +0300

    Initial Commit

--- src/main/java/com/benjaminsinzore/javafx_demo_gradle/HelloApplication.java
+++ src/main/java/com/benjaminsinzore/javafx_demo_gradle/HelloApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello Benjamin!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class
+                .getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 350);
+        stage.setTitle("Benjamin!");
         stage.setScene(scene);
         stage.show();
     }
